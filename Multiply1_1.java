80ccaa9242b9c325288d48b1239f14eeaae2b55a
KevinJtSong <30691179+KevinJtSong@users.noreply.github.com>
2017-08-03 19:51:46 +0800

    Add files via upload

---
+++ Multiply1_1.java
@@ -0,0 +1,10 @@
+package Day1;
+
+public class Multiply1_1 {
+	public static void main(String[] args) {
+		System.out.println(multiply(1.1,1.0));
+	}
+	public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+}
